236cda4b3d88a7025e8dff9005ab7f5864fd9178
shaikzahid0713 <shaikzahid0713@gmail.com>
2021-08-22 20:00:17 +0530

    Update Intro_using_oops.cpp

--- Data Structures and Algorithms/Trees/Intro_using_oops.cpp
+++ Data Structures and Algorithms/Trees/Intro_using_oops.cpp
@@ -1,1 +1,22 @@
-/* */
+/* 
+   Binary Trees: 
+      A tree consisting of at most two nodes. 
+
+      Binary Tree types: 
+         1. Full Binary Tree 
+               - Every node either has 0 or 2 children. 
+         2. Complete Binary Tree
+               - All levels are completely filled
+               - Last level has all nodes on left as possible.
+         3. Perfect Binary Tree 
+               - All leaf nodes are at the same level.
+         4. Balanced Binary Tree    
+               - Height of the tree at max of log2(n) for given n nodes.
+         5. Degenerate Binary Tree 
+               - Skew tree(Linked List)
+
+         
+
+
+
+*/
